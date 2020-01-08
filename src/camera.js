65ee45ae81bc95cea9066562d34b33982ac2bd44
zhouhang95 <765229842@qq.com>
2020-01-08 13:01:21 +0800

    camera base

---
+++ src/camera.js
@@ -0,0 +1,17 @@
+var Cameara = function() {
+    var o = {
+        phi: 0,
+        theta: 90,
+        r: 3,
+    }
+    o.getPosition = function() {
+        var phi = degreeToRadian(o.phi)
+        var theta = degreeToRadian(o.theta)
+        x = r * Math.cos(phi) * Math.sin(theta)
+        y = r * Math.sin(phi) * Math.sin(theta)
+        z = r * Math.cos(theta)
+        return [x, z, -y]
+    }
+
+    return o
+}
